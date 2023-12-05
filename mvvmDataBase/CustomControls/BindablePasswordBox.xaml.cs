a95f0d022bb2a14cfd493e5d1ef234803eba06c2
Ardan <ardandor120306@gmail.com>
2023-12-06 00:07:12 +0800

    New UI

---
+++ mvvmDataBase/CustomControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBase.CustomControls
+{
+    /// <summary>
+    /// Логика взаимодействия для BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        public static readonly DependencyProperty PasswordProperty= DependencyProperty.Register("Password",typeof(SecureString),typeof(BindablePasswordBox));
+        public SecureString Password {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChanged += OnPasswordchange;
+        }
+
+        private void OnPasswordchange(object sender, RoutedEventArgs e)
+        {
+            Password = txtPassword.SecurePassword;
+        }
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ mvvmDataBase/View/LoginView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mvvmDataBase.View
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        private void textLogin_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtLogin.Focus();
+        }
+        private void txtLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtLogin.Text) && txtLogin.Text.Length > 0)
+            {
+                textLogin.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textLogin.Visibility = Visibility.Visible;
+            }
+        }
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
