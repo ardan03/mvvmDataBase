fc78f410c065fb5f70e822fcd00a1020fa84cbf8
Ardan <ardandor120306@gmail.com>
2023-12-18 05:02:37 +0800

    Смена темы

--- mvvmDataBase/VewModel/SettingViewModel.cs
+++ mvvmDataBase/VewModel/SettingViewModel.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using mvvmDataBase.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace mvvmDataBase.VewModel
 {
-    internal class SettingViewModel
+    public class SettingViewModel : ViewModelBase
     {
+        private Stack<Window> windowStack = new Stack<Window>();
+        private string _themtxt="Светлая тема";
+        public string ThemTxt
+        {
+            get { return _themtxt; }
+            set
+            {
+                _themtxt = value;
+                OnPropertyChanged(nameof(ThemTxt));
+            }
+        }
+        private string thisThems = "Light";
+        public ICommand switchTems { get; set; }
+        public SettingViewModel()
+        {
+            switchTems = new RelsyCommand(SwithTems);
+        }
+
+        private void SwithTems(object obj)
+        {
+            if (thisThems == "Dark")
+            {
+                thisThems = "Light";
+                ThemTxt = "Светлая тема";
+                Application.Current.Resources.MergedDictionaries.Clear();
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("Thems/LightThems.xaml", UriKind.Relative) });
+            }
+            else
+            {
+                thisThems = "Dark";
+                ThemTxt = "Темная тема";
+                Application.Current.Resources.MergedDictionaries.Clear();
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("Thems/DarkTems.xaml", UriKind.Relative) });
+            }
+        }
     }
 }
--- mvvmDataBase/View/settungWindow.xaml.cs
+++ mvvmDataBase/View/settungWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mvvmDataBase.VewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,8 @@ namespace mvvmDataBase.View
         public settungWindow()
         {
             InitializeComponent();
+            DataContext = new SettingViewModel();
         }
+
     }
 }
